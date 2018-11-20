efb21117fcf316342d77af1791c42b1a52d0fce1
andrewtpalo <43764590+andrewtpalo@users.noreply.github.com>
2018-11-20 13:33:38 -0500

    Added cmd line args for printing graphs

--- kalmanfilter.js
+++ kalmanfilter.js
@@ -15,7 +15,7 @@ for(i=0;i<nsamples;i++){
 	velTrue.push(vtrue);
 }
 
-var xk_prev = math.matrix([[-1.3],[vtrue*0.5]]);
+var xk_prev = math.matrix([[0],[vtrue*0.5]]);
 var xk = [];
 var phi = math.matrix([[1, dt], [0, 1]]);
 
@@ -37,90 +37,109 @@ var test;
 var xkbufplot = [];
 var xkbufplotV = [];
 var instantV = [];
+var test1;
+var test2;
 instantV.push(0.0);
 for(i=0; i<nsamples; i++){
 	z = xtrue[i+1] + sigma_meas*Math.random();
 	z_buffer.push(z);
 	instantV.push((z-z_buffer[i-1])/dt);
-
+	//console.log(math.subset(xk_buffer, math.index(1,i)));
 	p1 = math.add(math.multiply(math.multiply(phi, p), math.transpose(phi)),q); //2x2
 	s = math.add(math.multiply(math.multiply(m, p1), mt),r); //1x1
 	k = math.multiply(math.multiply(p1, mt),math.inv(s)); //2x1
-
-	p = math.subtract(p1, math.multiply(math.multiply(k, m), p1)); //2x2
-	k = math.matrix([[math.subset(k, math.index(0))],[math.subset(k, math.index(1))]]);	
+	k = math.matrix([[math.subset(k, math.index(0))],[math.subset(k, math.index(1))]]);	;
+	p = math.subtract(p1,math.multiply(math.multiply(k, math.transpose(mt)), p1)); //2x2
 	test = math.multiply(k, math.subtract(z, math.multiply(math.multiply(m, phi), xk_prev)));
 	test = math.matrix([[math.subset(test, math.index(0))],[math.subset(test, math.index(1))]]);	
 	xk = math.add(math.multiply(phi, xk_prev), test);
-	console.log(xk);
 	xk_buffer = math.concat(xk_buffer, xk);
-	xkbufplot.push(math.subset(xk_buffer, math.index(0,i))+1.3);
+	xkbufplot.push(math.subset(xk_buffer, math.index(0,i)));
 	xkbufplotV.push(math.subset(xk_buffer, math.index(1,i)));
 	xk_prev = xk;
 }
 
-
-//PLOT
-var trace1 = {
-  x: t,
-  y: xtrue,
-  type: "scatter"
-};
-var trace2 = {
-  x: t,
-  y: z_buffer,
-  xaxis: "x2",
-  yaxis: "y2",
-  type: "scatter"
-};
-var trace3 = {
-  x: t,
-  y: xkbufplot,
-  xaxis: "x3",
-  yaxis: "y3",
-  type: "scatter"
-};
-var data = [trace1, trace2, trace3];
-var layout = {
-  xaxis: {domain: [0, 0.33]},
-  yaxis2: {anchor: "x2"},
-  xaxis2: {domain: [0.34, 0.66]},
-  yaxis3: {anchor: "x3"},
-  xaxis3: {domain: [0.67, 1]}
-};
-var graphOptions = {layout: layout, filename: "stacked-coupled-subplots", fileopt: "overwrite"};
-plotly.plot(data, graphOptions, function (err, msg) {
-    console.log(msg);
-}); 
-
-/*var trace1 = {
-  x: t,
-  y: velTrue,
-  type: "scatter"
-};
-var trace2 = {
-  x: t,
-  y: instantV,
-  xaxis: "x2",
-  yaxis: "y2",
-  type: "scatter"
-};
-var trace3 = {
-  x: t,
-  y: xkbufplotV,
-  xaxis: "x3",
-  yaxis: "y3",
-  type: "scatter"
-};
-var data = [trace1, trace2, trace3];
-var layout = {
-  xaxis: {domain: [0, 0.33]},
-  yaxis2: {anchor: "x2"},
-  xaxis2: {domain: [0.34, 0.66]},
-  yaxis3: {anchor: "x3"},
-  xaxis3: {domain: [0.67, 1]}
-};
-var graphOptions = {layout: layout, filename: "stacked-coupled-subplots", fileopt: "overwrite"};
-plotly.plot(data, graphOptions, function (err, msg) {
-    console.log(msg);
-});*/
+var display = process.argv[2];
+switch(display){
+		
+	//PLOT
+	case "-x":
+		var trace1 = {
+		  x: t,
+		  y: xtrue,
+		  name: "xTrue",
+		  type: "scatter"
+		};
+		var trace2 = {
+		  x: t,
+		  y: z_buffer,
+		  xaxis: "x2",
+		  yaxis: "y2",
+		  name: "Measured Position",
+		  type: "scatter"
+		};
+		var trace3 = {
+		  x: t,
+		  y: xkbufplot,
+		  xaxis: "x3",
+		  yaxis: "y3",
+		  name: "Filtered Position",
+		  type: "scatter"
+		};
+		var data = [trace1, trace2, trace3];
+		var layout = {
+		  yaxis: {anchor: "x", title: "xTrue"},
+		  xaxis: {domain: [0, 0.25], title: "time(s)"},
+		  yaxis2: {anchor: "x2", title: "measured Pos"},
+		  xaxis2: {domain: [0.375,0.625], title: "time(s)"},
+		  yaxis3: {anchor: "x3", title: "filtered Pos"},
+		  xaxis3: {domain: [0.75,1], title: "time(s)"}
+		};
+		var graphOptions = {layout: layout, filename: "stacked-coupled-subplots", fileopt: "overwrite"};
+		plotly.plot(data, graphOptions, function (err, msg) {
+			console.log(msg);
+		});
+		break;
+	case "-v":
+		var trace1 = {
+		  x: t,
+		  y: velTrue,
+		  xaxis: "x",
+		  yaxis: "y",
+		  name: "True Velocity",
+		  type: "scatter"
+		};
+		var trace2 = {
+		  x: t,
+		  y: instantV,
+		  xaxis: "x2",
+		  yaxis: "y2",
+		  name: "instantaneous Vel",
+		  type: "scatter"
+		};
+		var trace3 = {
+		  x: t,
+		  y: xkbufplotV,
+		  xaxis: "x3",
+		  yaxis: "y3",
+		  name: "Filtered Vel",
+		  type: "scatter"
+		};
+		var data = [trace1, trace2, trace3];
+		var layout = {
+		  yaxis: {anchor: "x", title: "velTrue"},
+		  xaxis: {domain: [0, 0.25], title: "time(s)"},
+		  yaxis2: {anchor: "x2", title: "instantaneous Vel"},
+		  xaxis2: {domain: [0.375,0.625], title: "time(s)"},
+		  yaxis3: {anchor: "x3", title: "filtered vel"},
+		  xaxis3: {domain: [0.75,1], title: "time(s)"}
+		};
+		var graphOptions = {layout: layout, filename: "stacked-coupled-subplots", fileopt: "overwrite"};
+		plotly.plot(data, graphOptions, function (err, msg) {
+			console.log(msg);
+		});
+		break;
+	default:
+		console.log("No Display Selected");
+		break;
+}
